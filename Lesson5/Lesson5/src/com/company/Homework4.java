4adc7dce5ba79a0dbaf1cfb188755e21fe93f205
Mariusz Mitrus <mariusz.mitrus1@gmail.com>
2019-10-23 11:39:02 +0200

    Added Math.abs method to find duplicates.

---
+++ Lesson5/Lesson5/src/com/company/Homework4.java
@@ -0,0 +1,111 @@
+package com.company;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Homework4 {
+
+    public static int [] insert(){
+        System.out.println("Add aray lenght");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int [] array = new int [n];
+        for (int i=0; i<n;i++){
+            System.out.println("Imput A[" + i + "]");
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+    public static void print (int [] array){
+        for (int i=0; i<array.length;i++){
+            System.out.println("Output " + array[i] );
+        }
+    }
+    //brute method- first thought
+    public static int [] firstDuplicateSearch(int [] array){
+        int n = array.length;
+        int [] ret = new int [n];
+        for (int i=0; i< n;i++){
+            for (int j=i+1; j<n; j++ ){
+                if (array [i] == array[j]){
+                    ret [i] =array [i];
+                    //i++;
+                }
+            }
+        }
+    return ret;
+    }
+
+    public static void quickSort(int[] arr, int start, int end){
+
+        int partition = partition(arr, start, end);
+
+        if(partition-1>start) {
+            quickSort(arr, start, partition - 1);
+        }
+        if(partition+1<end) {
+            quickSort(arr, partition + 1, end);
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end){
+        int pivot = arr[end];
+
+        for(int i=start; i<end; i++){
+            if(arr[i]<pivot){
+                int temp= arr[start];
+                arr[start]=arr[i];
+                arr[i]=temp;
+                start++;
+            }
+        }
+
+        int temp = arr[start];
+        arr[start] = pivot;
+        arr[end] = temp;
+
+        return start;
+    }
+
+    // Function to print duplicates
+    void printRepeating(int arr[], int size)
+    {
+        int i;
+        System.out.println("The repeating elements are : ");
+
+        for (i = 0; i < size; i++)
+        {
+            if (arr[ Math.abs(arr[i])] >= 0)
+                arr[ Math.abs(arr[i])] = -arr[ Math.abs(arr[i])];
+            else
+                System.out.print(Math.abs(arr[i]) + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] A = insert();
+        print(A);
+        long startTime = System.currentTimeMillis();
+        int [] firstDupicate = firstDuplicateSearch(A);
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Duplicates 1 : ");
+        print(firstDupicate);
+        System.out.println("Execution time in milliseconds: " + timeElapsed);
+        //QuickSort
+        System.out.println("Quick sort 2 : ");
+        startTime = System.currentTimeMillis();
+        quickSort(A,0,A.length-1);
+        endTime = System.currentTimeMillis();
+        timeElapsed = endTime - startTime;
+        System.out.println(Arrays.toString(A));
+        System.out.println("Execution time in milliseconds: " + timeElapsed);
+        //method no.2
+        startTime = System.currentTimeMillis();
+        Homework4 duplicate = new Homework4();
+        int [] arr = insert();
+        int arr_size = arr.length;
+        duplicate.printRepeating(arr, arr_size);
+        endTime = System.currentTimeMillis();
+        timeElapsed = endTime - startTime;
+        System.out.println("\nExecution time in milliseconds: " + timeElapsed);
+    }
+}
